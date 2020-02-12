38474a1b22783fc55d7ebab2082226c6013a8267
e <athink8@163.com>
2020-02-12 17:51:51 +0800

    '加入控制台信息'

--- themes/hexo-theme-matery-master/source/js/add1.js
+++ themes/hexo-theme-matery-master/source/js/add1.js
@@ -1,8 +1,14 @@
 $(function() {
 
+	/*  添加信息到控制面板*/
 	console.log("\n %c Athink_ %c https://github.com/athink8 \n",
 		"color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;")
-		/* console.log("\n %c APlayer v1.10.1 af84efb %c http://aplayer.js.org \n","color: #fadfa3; background: #030307; padding:5px 0;", */
+	console.log(
+		"\n\n\n%c亲爱的朋友们: \n  你们好,很高兴你能看见这段信息,本人为一名即将实习的Java程序员,\n"+
+		"在这人人技术都很强大的世界里,我不知我的归宿在何处,感觉学无止境,永远都学不完的知识让我很头大,\n"+
+		"但这些都不是重点,如果你能够给我次机会,我一定好好努力,好好加油! 谢谢!! \n 我的邮箱: athink8@163.com ",
+		"color: #202124; background: #ccffff; padding:6px;")
+
 	/* 浏览器搞笑标题 */
 	var OriginTitle = document.title;
 	var titleTime;
