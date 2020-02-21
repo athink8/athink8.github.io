8bc49cb7e53c7c92511b322cca9483a0f9d05c00
e <athink8@163.com>
2020-02-22 03:04:30 +0800

    Site updated: 2020-02-22 03:04:25

--- js/add1.js
+++ js/add1.js
@@ -1,15 +1,3 @@
-//在页面未加载完毕之前显示的loading Html自定义内容
-var _LoadingHtml = '<img src="https://cdn.jsdelivr.net/gh/athink8/athink8.github.io/medias/loding.gif" class="lodingPage"/>';
-//呈现loading效果
-document.write(_LoadingHtml);
-//监听加载状态改变
-document.onreadystatechange = completeLoading;
-//加载状态为complete时移除loading效果
-function completeLoading() {
-	if (document.readyState == "complete") {
-		$(".lodingPage").hide()
-	}
-}
 
 $(function() {
 
